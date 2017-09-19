8f3cc9c478f2401126783a54c53c0bd75f77ff7d
V <V@mail.ru>
2017-09-20 01:09:53 +0300

    refactoring

--- tests/TodoControllerShould/TodoControllerShould.cs
+++ tests/TodoControllerShould/TodoControllerShould.cs
@@ -5,24 +5,52 @@ using NTierTodo;
 using NTierTodo.Bll.Dto;
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Internal.Protocol;
+using Microsoft.AspNetCore.Sockets;
+using Microsoft.AspNetCore.Sockets.Client;
+using Microsoft.Rest.Azure;
 using Xunit;
 using static Newtonsoft.Json.JsonConvert;
 namespace TodoControllerShould
 {
-    public class TodoControllerShould
+    public class TodoControllerShould : IDisposable
     {
         private readonly HttpClient _client;
         private readonly ToDoDto _todo;
         private readonly string _root;
-
+        private readonly WebSocketClient _ws;
+        private readonly TestServer _server;
         public TodoControllerShould()
         {
-            _client = GetHttpClient();
+            var server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+
+            var client = server.CreateClient();
+            var ws = server.CreateWebSocketClient();
+
+            // client always expects json results
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            _client = client;
+
+            _ws = server.CreateWebSocketClient();
+
+            _server = server;
 
             _todo = new ToDoDto
             {
@@ -184,9 +212,10 @@ namespace TodoControllerShould
             Assert.Equal(_todo.Description, result.Description);
             Assert.NotEqual(_todo.IsComplite, result.IsComplite);
         }
+        
 
         [Fact]
-        private async Task MakeComplete()
+        public async Task MakeComplete()
         {
             var todo = new ToDoDto
             {
@@ -197,7 +226,7 @@ namespace TodoControllerShould
 
             create.EnsureSuccessStatusCode();
 
-            var id =(await FromContent(create.Content)).Id;
+            var id = (await FromContent(create.Content)).Id;
 
             var response = await _client.PostAsync(_root + id + "/MakeComplete", new StringContent(""));
 
@@ -227,19 +256,10 @@ namespace TodoControllerShould
             Assert.DoesNotContain(array, x => x.Id == _todo.Id);
         }
 
-        private HttpClient GetHttpClient()
+        public void Dispose()
         {
-            var server = new TestServer(WebHost.CreateDefaultBuilder()
-                .UseStartup<Startup>());
-
-            var client = server.CreateClient();
-
-            // client always expects json results
-            client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            return client;
+            _client.Dispose();
+            _server.Dispose();
         }
     }
 }
